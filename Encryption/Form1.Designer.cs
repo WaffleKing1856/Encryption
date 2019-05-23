05b9ba5a2717776fb597b022c5d3b4ef1ef7b23c
Matt <48110220+WaffleKing1856@users.noreply.github.com>
2019-05-23 10:45:38 -0400

    Add files via upload

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_e = new System.Windows.Forms.TextBox();
+            this.btn_e = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_code = new System.Windows.Forms.TextBox();
+            this.text_d = new System.Windows.Forms.TextBox();
+            this.btn_d = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_o = new System.Windows.Forms.TextBox();
+            this.btn_copy = new System.Windows.Forms.Button();
+            this.txt_codeO = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_e
+            // 
+            this.txt_e.Location = new System.Drawing.Point(199, 93);
+            this.txt_e.Name = "txt_e";
+            this.txt_e.Size = new System.Drawing.Size(504, 20);
+            this.txt_e.TabIndex = 0;
+            // 
+            // btn_e
+            // 
+            this.btn_e.Location = new System.Drawing.Point(12, 44);
+            this.btn_e.Name = "btn_e";
+            this.btn_e.Size = new System.Drawing.Size(127, 79);
+            this.btn_e.TabIndex = 2;
+            this.btn_e.Text = "Encrypt";
+            this.btn_e.UseVisualStyleBackColor = true;
+            this.btn_e.Click += new System.EventHandler(this.btn_e_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message To Encrypt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message To Decrypt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Code";
+            // 
+            // txt_code
+            // 
+            this.txt_code.Location = new System.Drawing.Point(198, 251);
+            this.txt_code.Name = "txt_code";
+            this.txt_code.Size = new System.Drawing.Size(166, 20);
+            this.txt_code.TabIndex = 6;
+            // 
+            // text_d
+            // 
+            this.text_d.Location = new System.Drawing.Point(198, 208);
+            this.text_d.Name = "text_d";
+            this.text_d.Size = new System.Drawing.Size(504, 20);
+            this.text_d.TabIndex = 8;
+            // 
+            // btn_d
+            // 
+            this.btn_d.Location = new System.Drawing.Point(12, 192);
+            this.btn_d.Name = "btn_d";
+            this.btn_d.Size = new System.Drawing.Size(127, 79);
+            this.btn_d.TabIndex = 9;
+            this.btn_d.Text = "Decrypt";
+            this.btn_d.UseVisualStyleBackColor = true;
+            this.btn_d.Click += new System.EventHandler(this.btn_d_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 347);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Output : ";
+            // 
+            // txt_o
+            // 
+            this.txt_o.Location = new System.Drawing.Point(391, 344);
+            this.txt_o.Multiline = true;
+            this.txt_o.Name = "txt_o";
+            this.txt_o.Size = new System.Drawing.Size(397, 68);
+            this.txt_o.TabIndex = 11;
+            // 
+            // btn_copy
+            // 
+            this.btn_copy.Location = new System.Drawing.Point(310, 379);
+            this.btn_copy.Name = "btn_copy";
+            this.btn_copy.Size = new System.Drawing.Size(75, 23);
+            this.btn_copy.TabIndex = 12;
+            this.btn_copy.Text = "Copy";
+            this.btn_copy.UseVisualStyleBackColor = true;
+            this.btn_copy.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txt_codeO
+            // 
+            this.txt_codeO.Location = new System.Drawing.Point(391, 418);
+            this.txt_codeO.Name = "txt_codeO";
+            this.txt_codeO.Size = new System.Drawing.Size(105, 20);
+            this.txt_codeO.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(284, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Code (Encrypt Only)";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(548, 293);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(119, 45);
+            this.btn_clear.TabIndex = 15;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_codeO);
+            this.Controls.Add(this.btn_copy);
+            this.Controls.Add(this.txt_o);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_d);
+            this.Controls.Add(this.text_d);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_code);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_e);
+            this.Controls.Add(this.txt_e);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_e;
+        private System.Windows.Forms.Button btn_e;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_code;
+        private System.Windows.Forms.TextBox text_d;
+        private System.Windows.Forms.Button btn_d;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_o;
+        private System.Windows.Forms.Button btn_copy;
+        private System.Windows.Forms.TextBox txt_codeO;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ Encryption/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txt_o.Text);
+            MessageBox.Show("Successfully Copied!");
+        }
+
+        private void btn_e_Click(object sender, EventArgs e)
+        {
+            string encrypt = txt_e.Text; //Creates Variable from textbox's string
+            int ran = RandomNumber(1, 11); //generates a random number from 1-10
+            DoEncryption(encrypt, ran); //Function encryption
+            
+
+        }
+
+
+        public string SecretCode(int x)
+        {
+            string done = null;
+            //simple conversion from number to 
+            //random phrase
+
+            if(x == 1)
+            {
+                done = "lol";
+            }
+            else if(x == 2)
+            {
+                done = "oof";
+            }
+            else if(x == 3)
+            {
+                done = "epic";
+            }
+            else if(x == 4)
+            {
+                done = "dab";
+            }
+            else if(x == 5)
+            {
+                done = "whip";
+            }
+            else if(x == 6)
+            {
+                done = "wes";
+            }
+            else if (x == 7)
+            {
+                done = "minecraft";
+            }
+            else if (x == 8)
+            {
+                done = "code";
+            }
+            else if (x == 9)
+            {
+                done = "gamer";
+            }
+            else if (x == 10)
+            {
+                done = "roblox";
+            }
+
+            return done;
+        }
+
+        private void DoEncryption(string encrypt, int ran)
+        {
+            string final = ""; //variable declaration
+            for(int i = 0; i < encrypt.Length; i++) //For loop to check every charecter 
+            {
+                int x = encrypt[i]; //takes the char value of every index position
+                x += ran; //Adds the random number to the char value
+                Char y = (char)x; // converting back to a letter 
+                string f = y.ToString(); //converting the char to a string 
+                final = final + f; //adding the string together with eveything else
+                
+            }
+            txt_codeO.Text = SecretCode(ran); //Calls the function to change the number into a secret word and add text 
+            txt_o.Text = final; //adds the text to the textbox where the output is
+
+        }
+
+        private void btn_d_Click(object sender, EventArgs e)
+        {
+            string code = txt_code.Text; //Creates Variable from the code textbox
+            string toDecrypt = text_d.Text; //Creates Variable from the encrypted text
+            DoDecryption(code, toDecrypt); //Calls decryption function
+        }
+
+        private void DoDecryption(string inC, string toD)
+        {
+            string final = ""; //Variable delaration
+            int code = SecretBack(inC); //calls the oposite of the number to phrase converter
+            for (int i = 0; i < toD.Length; i++) //Does these steps to every charecter
+            {
+                int temp = toD[i]; //creates a temporary variable of every index in the word
+                                   //and converts to char value
+                temp -= code; //subtract the code from the char value
+                final += (char)temp; //creates a final string for the decrypted phrase
+            }
+            txt_o.Text = final; //sets the textbox equal to the final phrase
+            
+        }
+
+        public int SecretBack(string code)
+        {
+            int done = 0; //simple if statements converting phrases back to numbers
+
+            if(code == "lol")
+            {
+                done = 1;
+            }
+            else if(code == "oof")
+            {
+                done = 2;
+            }
+            else if (code == "epic")
+            {
+                done = 3;
+            }
+            else if (code == "dab")
+            {
+                done = 4;
+            }
+            else if (code == "whip")
+            {
+                done = 5;
+            }
+            else if (code == "wes")
+            {
+                done = 6;
+            }
+            else if (code == "minecraft")
+            {
+                done = 7;
+            }
+            else if (code == "code")
+            {
+                done = 8;
+            }
+            else if (code == "gamer")
+            {
+                done = 9;
+            }
+            else if (code == "roblox")
+            {
+                done = 10;
+            }
+            return done;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_codeO.ResetText(); //Clears the Bottom output box
+            txt_o.ResetText(); //Clears the Top output box
+        }
+    }
+}
